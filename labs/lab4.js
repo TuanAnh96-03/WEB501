5458bfc7a48bac3b05a7338d63a1bf8c9f3b9bd6
anhtt <tuananh03122006@gmail.com>
2025-11-12 14:25:55 +0700

    hoan thanh lab4

--- labs/lab4.js
+++ labs/lab4.js
@@ -16,29 +16,33 @@
 // const [pass, ...userInfo]=user;
 // console.log(userInfo);
 
+
+// Bài tập Array Destructuring
 // Bai 1
-// function getFirstLast(array) {
-// if(!Array.isArray(array) || array.length ===0 ){
-//     return[];
-// };
-// return array.slice(0,1).concat(array.slice(-1)) // Your code here
-// }
+function getFirstLast(array) {
+if(!Array.isArray(array) || array.length ===0 ){
+    return[];
+};
+return array.slice(0,1).concat(array.slice(-1)) // Your code here
+}
 
-// console.log(getFirstLast([1, 2, 3, 4])); // [1, 4]
+console.log(getFirstLast([1, 2, 3, 4])); // [1, 4]
 
-// // Bài 2: Hoán đổi vị trí phần tử thứ 2 và thứ 4 trong mảng
+// Bài 2: Hoán đổi vị trí phần tử thứ 2 và thứ 4 trong mảng
 
-// function swapElements(arr) {
-//   if (!Array.isArray(arr)) return [];
-//   const res = [...arr];
-//   if (res.length >= 4) {
-//     [res[1], res[3]] = [res[3], res[1]];
-//   }
-//   return res;
-// }
+function swapElements(arr) {
+  if (!Array.isArray(arr)) return [];
+  const res = [...arr];
+  if (res.length >= 4) {
+    [res[1], res[3]] = [res[3], res[1]];
+  }
+  return res;
+}
 
-// console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
+console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
 
+
+// Oject Destructuring
 // const person = {
 //   name: "hoadv",
 //   age: 30,
@@ -48,24 +52,49 @@
 // const { name, age } = person;
 // console.log(name, age); // 'hoadv' 30
 
-// //Bài 3: Viết hàm extract thông tin user
-// const user = {
-//   id: 1,
-//   personalInfo: {
-//     name: "javascript",
-//     contact: {
-//       email: "javascript@email.com",
-//       phone: "123-456-7890",
-//     },
-//   },
-// };
 
-// function getUserInfo(user) {
-//      // Your code here
-// }
+// Bài tập 2: Object Destructuring
+//Bài 3: Viết hàm extract thông tin user
+const user = {
+  id: 1,
+  personalInfo: {
+    name: "javascript",
+    contact: {
+      email: "javascript@email.com",
+      phone: "123-456-7890",
+    },
+  },
+};
+function getUserInfo(userData) {
+  const {
+  personalInfo:{
+    name,
+    contact: {email},
+  },
 
-// console.log(getUserInfo(user));
-// // { name: 'javascript', email: 'javascript@email.com' }
+}=userData;
+return {name,email};// Your code here
+}
+console.log(getUserInfo(user));
+// { name: 'javascript', email: 'javascript@email.com' }
+
+// Bài 2: Viết hàm với tham số mặc định
+
+function createProduct(
+  { name,
+    price, 
+    category = "general", 
+    inStock = true }) {
+  return{
+  name,
+  price,
+  category,
+  inStock  } ;   // Your code here
+}
+
+const product = createProduct({ name: "Laptop", price: 999 });
+console.log(product);
+// { name: 'Laptop', price: 999, category: 'general', inStock: true }
 
 
 
@@ -103,28 +132,55 @@
 //     // code luôn chạy
 //   });
   // Tạo Promise
-function fetchUserData(userId) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (userId > 0) {
-        resolve({ id: userId, name: "hoa dv" });
-      } else {
-        reject("Invalid user ID");
-      }
-    }, 1000);
-  });
+// function fetchUserData(userId) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (userId > 0) {
+//         resolve({ id: userId, name: "hoa dv" });
+//       } else {
+//         reject("Invalid user ID");
+//       }
+//     }, 1000);
+//   });
+// }
+// fetchUserData(1)
+//   .then((user) => {
+//     console.log("User data:", user);
+//     return user.name;
+//   })
+//   .then((userName) => {
+//     console.log("User name:", userName);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   })
+//   .finally(() => {
+//     console.log("Operation completed");
+//   });
+
+  // Bài tập 3: Promises
+  // Bài 1: Viết hàm delay trả về Promise
+
+function delay(ms) {
+  return new Promise(resolve =>{
+    setTimeout(()=>{
+      resolve();
+    },ms)
+  })
+ // Your code here
 }
-fetchUserData(1)
-  .then((user) => {
-    console.log("User data:", user);
-    return user.name;
-  })
-  .then((userName) => {
-    console.log("User name:", userName);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  })
-  .finally(() => {
-    console.log("Operation completed");
-  });
+
+delay(2000).then(() => console.log("2 seconds passed"));
+
+
+
+// Bài 2: Viết hàm fetchMultipleData
+
+function fetchMultipleData(urls) {
+    
+  // Your code here
+}
+
+fetchMultipleData(["/api/user/1", "/api/user/2"]).then((users) =>
+  console.log(users)
+);
